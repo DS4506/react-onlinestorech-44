b211dfa7711cf1a7ef15d65d5bcd13b86c8673aa
DS4506 <davidsmith4506@gmail.com>
2024-02-15 21:53:05 -0600

    page switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar';
-import Footer from './footer';
-import Catalog from './pages/catalog';
-import Home from './pages/home';
-import About from './pages/about';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./footer";
+import Catalog from "./pages/catalog";
+import Home from "./pages/home";
+import About from "./pages/about";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <h1> Dave is using react</h1>
-    <Navbar></Navbar>
-    <Footer></Footer>
-    <Home/>
-    <About/>
-    <Catalog></Catalog>
-    
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1> Dave is using react</h1>
+        <Navbar></Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
